db798e07b5d78494f0e71658d3627342ab3b1c27
Ankit <akjain6067.aj@gmail.com>
2021-06-29 20:45:44 +0530

    Level component done

--- src/components/Level.jsx
+++ src/components/Level.jsx
@@ -1,44 +1,46 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import "./../scss/Level.scss";
 
 const Level = (props) => {
   const dispatch = useDispatch();
+
+  const currLevel = useSelector((state) => state.level);
 
   const changeLevel = (level) => {
     dispatch({ type: "LEVEL_CHANGE", level: level, pos: level * level - 1 });
   };
 
+  const isActive = (level) => {
+    return level === currLevel ? "is-active" : "";
+  };
+
   return (
-    <>
-      <div className="container">
-        <div>
-          <div
-            className="label"
-            onClick={() => {
-              changeLevel(3);
-            }}
-            className="lavel-name"
-          >
-            Easy
-          </div>
-          <div
-            className="lavel-name"
-            onClick={() => {
-              changeLevel(4);
-            }}
-          >
-            Medium
-          </div>
-          <div
-            className="lavel-name"
-            onClick={() => {
-              changeLevel(5);
-            }}
-          >
-            Hard
-          </div>
-        </div>
+    <div className="levels">
+      <div
+        className={`level-name ${isActive(3)}`}
+        onClick={() => {
+          changeLevel(3);
+        }}
+      >
+        Easy
       </div>
-    </>
+      <div
+        className={`level-name ${isActive(4)}`}
+        onClick={() => {
+          changeLevel(4);
+        }}
+      >
+        Medium
+      </div>
+      <div
+        className={`level-name ${isActive(5)}`}
+        onClick={() => {
+          changeLevel(5);
+        }}
+      >
+        Hard
+      </div>
+    </div>
   );
 };
 
